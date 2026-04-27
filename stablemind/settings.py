4b1576901eddb56cccb2e6ecab3593fea9ba0107
hasan-mavlonov <hmavlanov79@gmail.com>
2026-04-27 20:34:02 +0800

    fix supabase connection ssl

--- stablemind/settings.py
+++ stablemind/settings.py
@@ -39,8 +39,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
+
+    DATABASES["default"]["OPTIONS"] = {
+        "sslmode": "require"
+    }
+
 else:
     DATABASES = {
         "default": {
