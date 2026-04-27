e0dda7c7a22aa447075a010c8ea56dc5f013243a
hasan-mavlonov <hmavlanov79@gmail.com>
2026-04-27 19:30:24 +0800

    added secret key

--- stablemind/settings.py
+++ stablemind/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-me')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-9pm!z^(=t_&fihs$u%)3doe@a&o0d6mbu77ti=_w-yy7#n(6q%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
