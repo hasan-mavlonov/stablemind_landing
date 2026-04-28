94391daacd359c31e33e15e23d7b91434ad19a51
hasan-mavlonov <hmavlanov79@gmail.com>
2026-04-28 13:31:10 +0800

    edited urls.py

--- stablemind/urls.py
+++ stablemind/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
 from core.views import landing_page, careers_page
 
 urlpatterns = [
@@ -10,5 +11,5 @@ urlpatterns = [
     path('careers/', careers_page, name='careers'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# IMPORTANT: serve media on Render (works even in production single-instance)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
